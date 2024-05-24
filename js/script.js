14a66c7e93600aa2a66c8bd7a40002ee5aa8f300
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-05-24 15:50:14 +0000

    add webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: May 2024
 // This file contains the JS functions for index.html
+/*
+ if (localStorage.hits) {
+  localStorage.hits = Number(localStorage)
+} else {
+  localStorage = 1
+}
+document.getElementById("result").innerHTML = localStorage.hits
+ */
+
+"use strict"
+
+let clickCount = 0
+clickCount = localStorage.getItem(clickCount) || 0
+
+function updateCookieCount() {
+  clickCount++
+  localStorage.setItem("clickCount", clickCount)
+  document.getElementById("result").innerHTML = "Counter : " + clickCount
+}
+
